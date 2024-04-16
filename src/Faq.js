a3c29921f4d8829d5715411b8026f9dfada1d3fc
Syed Muhammad ALi Raza <smaliraza412@gmail.com>
2024-04-16 12:18:38 +0500

    faq updated

--- src/Faq.js
+++ src/Faq.js
@@ -8,13 +8,12 @@ const Faq = () => {
     };
 
     return (
-        <div className='container px-20 h-[100vh]'>
+        <div className='container px-20 '>
             <h2 className='text-5xl font-bold text-center py-12'>Frequently Asked Questions</h2>
 
             <div
-                className={`question1   bg-slate-600  mx-32 flex justify-between px-12 py-3 my-2 hover:bg-slate-400 ${
-                    openQuestion === 1 ? 'open' : ''
-                }`}
+                className={`question1   bg-slate-600  mx-32 flex justify-between px-12 py-3 my-2 hover:bg-slate-400 ${openQuestion === 1 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(1)}
             >
                 <div className='left'>What is Netflix</div>
@@ -29,9 +28,8 @@ const Faq = () => {
             )}
 
             <div
-                className={`question2 bg-slate-600  mx-32 flex justify-between px-12 py-3 my-2 hover:bg-slate-400  ${
-                    openQuestion === 2 ? 'open' : ''
-                }`}
+                className={`question2 bg-slate-600  mx-32 flex justify-between px-12 py-3 my-2 hover:bg-slate-400  ${openQuestion === 2 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(2)}
             >
                 <div className='left'>How much does Netflix cost?</div>
@@ -46,9 +44,8 @@ const Faq = () => {
             )}
 
             <div
-                className={`question3  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${
-                    openQuestion === 3 ? 'open' : ''
-                }`}
+                className={`question3  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${openQuestion === 3 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(3)}
             >
                 <div className='left'>Where can I watch?</div>
@@ -66,10 +63,9 @@ const Faq = () => {
                 </div>
             )}
 
-<div
-                className={`question4  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${
-                    openQuestion === 4 ? 'open' : ''
-                }`}
+            <div
+                className={`question4  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${openQuestion === 4 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(3)}
             >
                 <div className='left'>Where can I watch?</div>
@@ -87,10 +83,9 @@ const Faq = () => {
                 </div>
             )}
 
-<div
-                className={`question5  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${
-                    openQuestion === 5 ? 'open' : ''
-                }`}
+            <div
+                className={`question5  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${openQuestion === 5 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(5)}
             >
                 <div className='left'>Where can I watch?</div>
@@ -108,10 +103,9 @@ const Faq = () => {
                 </div>
             )}
 
-<div
-                className={`question6  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${
-                    openQuestion === 6 ? 'open' : ''
-                }`}
+            <div
+                className={`question6  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${openQuestion === 6 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(6)}
             >
                 <div className='left'>Where can I watch?</div>
@@ -129,11 +123,9 @@ const Faq = () => {
                 </div>
             )}
 
-
-<div
-                className={`question7  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${
-                    openQuestion === 7 ? 'open' : ''
-                }`}
+            <div
+                className={`question7  my-2  mx-32 bg-slate-600  flex justify-between px-12 py-3 hover:bg-slate-400 ${openQuestion === 7 ? 'open' : ''
+                    }`}
                 onClick={() => handleQuestionClick(7)}
             >
                 <div className='left'>Where can I watch?</div>
@@ -150,6 +142,19 @@ const Faq = () => {
                     Take Netflix with you anywhere.
                 </div>
             )}
+
+        <div className='text-center text-2xl my-10' >
+                Ready to watch? Enter your email to create or restart your membership.
+                
+            
+
+            </div>
+            <div className='text-center flex justify-center item-center gap-5 my-5'>
+                <input type='text' className='btn p-2 rounded-md bg-transparent border-slate-600 border-2 text-blue-900 placeholder-gray-400 ' placeholder='Email Address'/>
+                <button className='btn bg-red-700 rounded-lg text-white px-4 py-2'>Get Started 
+        {/* FontAwesomeIcon icon={faGreaterThan} /> */}
+                </button>
+            </div>
         </div>
     );
 };
