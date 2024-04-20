676ef70fee4d22686103e5a75a7b0841cda481f2
Syed Muhammad Ali Raza <smaliraza412@gmail.com>
2024-04-20 21:35:09 +0500

    section update

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Section2 = () => {
     return (
         <div>
-            <div className='flex gap-7'>
+            <div className='flex gap-7 h-[100vh]'>
 
                 <div className='left '>
                     <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" />
@@ -29,10 +29,10 @@ const Section2 = () => {
 
                 <div className='right flex justify-center items-center basis-[50%]'>
                     <div className='grid justify-center items-center'>
-                        <h2 className='text-4xl font-semibold'>
+                        <h2 className='text-4xl font-semibold w-[80%]'>
                             Download your shows to watch offline
                         </h2>
-                        <p className='text-xl'>
+                        <p className='text-xl w-[70%] my-4'>
                             Save your favorites easily and always have something to watch.
                         </p>
                     </div>
