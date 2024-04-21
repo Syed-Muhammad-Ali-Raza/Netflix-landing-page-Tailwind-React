50d61288d9a19c5aa14404f5bc28f44088195cd7
Syed Muhammad Ali Raza <smaliraza412@gmail.com>
2024-04-21 20:20:45 +0500

    shadow fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <div className='bg-cover bg-center bg-no-repeat ' style={{ backgroundImage: `url(https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/291d18e4-fcd3-45ec-9843-6c39247bc0ae/PK-en-20240408-popsignuptwoweeks-perspective_alpha_website_large.jpg)` }}>
-        <div className='blur-0'>
+        <div className=' bg-[rgba(0,0,0,0.5)] over w-[100%] h-[100vh]'>
           <Header />
           <Hero />
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import netflixlogo from './netflixlogi.png'
 const Header = () => {
     return (
         <>
@@ -7,8 +7,12 @@ const Header = () => {
             <div className='flex justify-between items-center '>
                 <div className='image px-12'> 
                     <img src="./netflixlogi.png  " width="200px" />
+                    {/* <div className='bg-[rgba(0,0,0,0.5)] over w-[100%] h-[100vh]'>
+
+                </div> */}
 
                 </div>
+              
                 <div className='button px-11'>
                     <button className='btn p-2 rounded-md bg-red-600 text-white'>Sign in</button>
                 </div>
